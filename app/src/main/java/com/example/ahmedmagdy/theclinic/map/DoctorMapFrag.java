9a93e8b0689705b363e58fe333bd87cf2ee30608
mohamed <mohamed.sobhy.abdelmageed@outlook.com>
2019-02-08 09:40:36 +0200

    info direction window step 1

--- app/src/main/java/com/example/ahmedmagdy/theclinic/map/DoctorMapFrag.java
+++ app/src/main/java/com/example/ahmedmagdy/theclinic/map/DoctorMapFrag.java
@@ -3,6 +3,7 @@ package com.example.ahmedmagdy.theclinic.map;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,6 +13,8 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -33,9 +36,17 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
 import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.model.DirectionsResult;
+
+import android.location.LocationListener;
 
 import java.util.ArrayList;
 
@@ -45,9 +56,9 @@ import static com.example.ahmedmagdy.theclinic.map.Constants.PERMISSIONS_REQUEST
 import static com.example.ahmedmagdy.theclinic.map.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
 
 
-public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
+public class DoctorMapFrag extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
-    private static final String TAG = "UserListFragment";
+    private static final String TAG = "DoctorMapFragment";
     public static int cunterRefresh = 0;
 
     //widgets
@@ -64,6 +75,42 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
     private MyClusterManagerRenderer mClusterManagerRenderer;
     private ArrayList<ClusterMarker> mClusterMarkers = new ArrayList<>();
     private ArrayList<UserLocation> mUserLocs = new ArrayList<>();
+    private GeoApiContext mGeoApiContext = null;
+    /*direction*/
+    public static final int LOCATION_UPDATE_MIN_DISTANCE = 10;
+    public static final int LOCATION_UPDATE_MIN_TIME = 5000;
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                myLat = location.getLatitude();
+                myLng= location.getLongitude();
+                System.out.println(TAG + " listener my" + myLat + myLng);
+                //drawMarker(location);
+                addMapMarkers();
+                mLocationManager.removeUpdates(mLocationListener);
+            } else {
+                System.out.println(TAG + " listener " + "Location is null");
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+    private LocationManager mLocationManager;
+    /*direction*/
 
 
     public static DoctorMapFrag newInstance() {
@@ -83,7 +130,6 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
         }
 
 
-
     }
 
     @Nullable
@@ -95,6 +141,8 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
         mMapView = view.findViewById(R.id.user_list_map);
 
         checkMapServices();
+
+        mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
         initGoogleMap(savedInstanceState);
 
@@ -150,27 +198,27 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
 
             for (UserLocation userLocation : mUserLocs) {
 
-                if(     userLocation.getCmdoctorid()!=null &&
-                        userLocation.getCmname()!=null &&
-                        userLocation.getCmdoctorspecialty()!=null &&
-                        userLocation.getCmDoctorGander()!=null
+                if (userLocation.getCmdoctorid() != null &&
+                        userLocation.getCmname() != null &&
+                        userLocation.getCmdoctorspecialty() != null &&
+                        userLocation.getCmDoctorGander() != null
                         ) {
-                    System.out.println(TAG + "addMapMarkers: location: " +
+                    /*System.out.println(TAG + "addMapMarkers: location: " +
                             Double.parseDouble(userLocation.getCmlatitude()) +
                             "," + Double.parseDouble(userLocation.getCmlongitude()) +
                             "," + userLocation.getCmdoctorid() +
                             ",gender: " + userLocation.getCmDoctorGander() +
                             ",speci: " + userLocation.getCmdoctorspecialty() +
                             "," + userLocation.getCmname()
-                    );
+                    );*/
                     try {
                         int avatar = 0; // set the default avatar
-                        if(userLocation.getCmDoctorGander().equals("Male")){
+                        if (userLocation.getCmDoctorGander().equals("Male")) {
                             avatar = R.drawable.doc_male_icon;
-                            System.out.println("avatarMale: "+avatar);
-                        }else{
+                            System.out.println("avatarMale: " + avatar);
+                        } else {
                             avatar = R.drawable.doc_female_icon;
-                            System.out.println("avatarFemale: "+avatar);
+                            System.out.println("avatarFemale: " + avatar);
                         }
                         String avatarUrl = null; // set the default avatar
                         try {
@@ -205,6 +253,14 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
 
             setCameraView();
 
+            //add my marker
+            LatLng gps = new LatLng(myLat, myLng);
+            System.out.println(TAG+" gps marker "+gps);
+            if( gps != null){
+                mGoogleMap.addMarker(new MarkerOptions()
+                        .position(gps)
+                        .title(getString(R.string.mylocation)));
+            }
 
         } else {
             //for doctor profile
@@ -213,11 +269,11 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
 
     }
 
-    private void resetMap(){
-        if(mGoogleMap != null) {
+    private void resetMap() {
+        if (mGoogleMap != null) {
             mGoogleMap.clear();
 
-            if(mClusterManager != null){
+            if (mClusterManager != null) {
                 mClusterManager.clearItems();
             }
 
@@ -247,8 +303,8 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
             public void onComplete(@NonNull Task<Location> task) {
                 if (task.isSuccessful()) {
                     final Location location = task.getResult();
-                    myLat =  location.getLatitude();
-                    myLng =  location.getLongitude();
+                    myLat = location.getLatitude();
+                    myLng = location.getLongitude();
                     //overall map view will 0.4 = 0.2 , 0.2
                     double mBottomBoundry = myLat - 0.1;
                     double mLeftBoundry = myLng - 0.1;
@@ -279,6 +335,11 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
 
+        if (mGeoApiContext == null) {
+            mGeoApiContext = new GeoApiContext.Builder()
+                    .apiKey(getString(R.string.google_api_key)).build();
+        }
+
         mMapView.onCreate(mapViewBundle);
 
         mMapView.getMapAsync(this);
@@ -305,6 +366,7 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
         if (checkMapServices()) {
             if (mLocationPermissionGranted) {
                 getLocationPermission();
+                getCurrentLocation();
                 Toast.makeText(getContext(), "Permission granted Successfully",
                         Toast.LENGTH_LONG).show();
             } else {
@@ -340,11 +402,14 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
         map.setMyLocationEnabled(true);
         mGoogleMap = map;
         addMapMarkers();
+        mGoogleMap.setOnInfoWindowClickListener(this);
+        getCurrentLocation();
     }
 
     @Override
     public void onPause() {
         mMapView.onPause();
+        mLocationManager.removeUpdates(mLocationListener);
         super.onPause();
     }
 
@@ -432,10 +497,117 @@ public class DoctorMapFrag extends Fragment implements OnMapReadyCallback {
         return false;
     }
 
+    @Override
+    public void onInfoWindowClick(final Marker marker) {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Do you want to get direction to " + marker.getTitle() + " ?")
+                .setCancelable(true)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        calculateDirections(marker);
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+
+    private void calculateDirections(Marker marker) {
+        System.out.println(TAG + "calculateDirections: calculating directions." +
+                " .. myLocLatLng >>> " + myLat + "," + myLng);
+
+        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
+                marker.getPosition().latitude,
+                marker.getPosition().longitude
+        );
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
+
+        directions.alternatives(true);
+        directions.origin(
+                new com.google.maps.model.LatLng(
+                        myLat,
+                        myLng
+                )
+        );
+        System.out.println(TAG + "calculateDirections: destination: " + destination.toString());
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                System.out.println(TAG + "onResult: routes: " + result.routes[0].toString());
+                System.out.println(TAG + "onResult: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                System.out.println(TAG + "onFailure: " + e.getMessage());
+
+            }
+        });
+    }
+
     /*check map permissions */
     /*test check permission*/
 
     /*refresh*/
     /*refresh*/
 
+    private void getCurrentLocation() {
+        boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Location location = null;
+        if (!(isGPSEnabled || isNetworkEnabled))
+            Snackbar.make(mMapView, R.string.error_location_provider, Snackbar.LENGTH_INDEFINITE).show();
+        else {
+            if (isNetworkEnabled) {
+                if (ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(getContext(),
+                                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                        LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, mLocationListener);
+                location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+
+            if (isGPSEnabled) {
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, mLocationListener);
+                location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+        }
+        if (location != null) {
+            System.out.println(TAG+"getCurrentLocation"+ location.getLatitude()+
+                    location.getLongitude());
+            //drawMarker(location);
+            //addMapMarkers();
+        }
+    }
+
+    private void drawMarker(Location location) {
+        if (mGoogleMap != null) {
+            mGoogleMap.clear();
+            LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
+            mGoogleMap.addMarker(new MarkerOptions()
+                    .position(gps)
+                    .title("Current Position"));
+            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 12));
+        }
+
+    }
 }
